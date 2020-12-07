c0fd86f2acc657d42d7c1b1f6ada5b669661c310
Gia Khiem Dinh <khiemgamo123@gmail.com>
2020-12-07 19:27:38 +0700

    "Floating point comparision" kata's Solution

---
+++ Floating point comparison.py
@@ -0,0 +1,17 @@
+'''
+You have:
+
+[+] a float value that comes from a computation and may have accumulated errors up to ±0.001
+[+] a reference value
+[+] a function approx_equals that compare the two values taking into account loss of precision; the function should return True if and only if the two values are close to each other, the maximum allowed difference is 0.001
+The function is bugged and sometimes returns wrong results.
+
+Your task is to correct the bug.
+'''
+
+#solution:
+def approx_equals(a, b):
+    if (abs(a-b) <= 0.001):
+        return True
+    else:
+        return False
