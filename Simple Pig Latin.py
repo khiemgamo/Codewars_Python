dabb7e4e1a7d8bab2a8f421da40ddd9df019d4c8
Gia Khiem Dinh <khiemgamo123@gmail.com>
2020-12-10 00:50:45 +0700

    "Simple Pig Latin" kata's solution

---
+++ Simple Pig Latin.py
@@ -0,0 +1,19 @@
+'''
+[+]Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+[+]Examples:
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+pig_it('Hello world !')     # elloHay orldway !
+'''
+
+#solution:
+def pig_it(text):
+    word = []
+    specchar = ["!", "?", "."]
+    pig = text.split()
+    for i in range(len(pig)):
+        if pig[i][0] in specchar:
+            word.append(pig[i])
+        else:
+            word.append(pig[i].replace(pig[i][0],"",1) + pig[i][0] + "ay")
+    return " ".join(word)
