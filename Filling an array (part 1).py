f9df48126c70b169a0ad0821487503cabe141abe
Gia Khiem Dinh <khiemgamo123@gmail.com>
2020-12-13 23:26:12 +0700

    "Filling an array (part 1)" kata's Solution

---
+++ Filling an array (part 1).py
@@ -0,0 +1,16 @@
+'''
+[+]We want an array, but not just any old array, an array with contents!
+
+[+]Write a function that produces an array with the numbers 0 to N-1 in it.
+
+[+]For example, the following code will result in an array containing the numbers 0 to 4:
+arr(5) // => [0,1,2,3,4]
+'''
+
+#solution:
+def arr(n=0): 
+    return [num for num in range(n)]
+
+#solution 2:
+def arr(n=0):
+    return list(range(n))
