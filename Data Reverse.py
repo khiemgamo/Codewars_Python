cd38274562ff8d9afb3559367ac727a076d6af3c
Gia Khiem Dinh <khiemgamo123@gmail.com>
2020-12-20 18:23:09 +0700

    "Data Reverse" kata's Solution

---
+++ Data Reverse.py
@@ -0,0 +1,27 @@
+'''
+[+]A stream of data is received and needs to be reversed.
+
+[+]Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+  11111111  00000000  00001111  10101010
+   (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+  10101010  00001111  00000000  11111111
+   (byte4)   (byte3)   (byte2)   (byte1)
+   
+[+]The total number of bits will always be a multiple of 8.
+
+[+]The data is given in an array as such:
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+'''
+
+#solution:
+def data_reverse(data):
+    list = []
+    rev = []
+    for i in range(8,len(data)+1,8):
+        k = i - 8
+        rev.append(data[k:i])
+    for i in range(len(rev)):
+        list += rev[::-1][i]
+    return list
